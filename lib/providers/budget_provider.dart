12e1f7fe8aa70c10d485c7b1ce80f18304088876
Arvin-Rsl <rasulzadeh.arvin@gmail.com>
2026-07-04 16:03:00 +0330

    feat(provider): initialize BudgetProvider class with hardcoded categories

---
+++ lib/providers/budget_provider.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:personal_budget_app/models/budget_models.dart';
+
+class BudgetProvider extends ChangeNotifier {
+  final List<Category> _categories = [
+    Category(id: '1', name: 'Food', allocatedBudget: 500.0),
+    Category(id: '2', name: 'Transportation', allocatedBudget: 100.0),
+    Category(id: '3', name: 'Sports', allocatedBudget: 80.0),
+    Category(id: '4', name: 'Clothes', allocatedBudget: 150.0),
+    Category(id: '5', name: 'Household Essentials', allocatedBudget: 80.0),
+    Category(id: '6', name: 'Having Fun', allocatedBudget: 150.0),
+    Category(id: '7', name: 'Savings', allocatedBudget: 1000.0),
+  ];
+
+  List<Transaction> _transactions = [];
+
+  List<Category> get categories => _categories;
+
+  List<Transaction> get transactions => _transactions;
+}
