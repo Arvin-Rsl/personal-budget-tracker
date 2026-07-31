ccfd994d3cf149faac95aaaa2c21d3c65db72f06
Arvin-Rsl <rasulzadeh.arvin@gmail.com>
2026-07-31 10:11:40 +0330

    docs: add TODOs for Income and Transfer models

--- lib/models/budget_models.dart
+++ lib/models/budget_models.dart
@@ -1,3 +1,9 @@
+// TODO: Add Income model - tracks money coming in, immutable ledger entry
+
+// TODO: Add FundPool enum + Transfer model (tracks money movement between
+// Unallocated Funds, Savings, and category/month budgets).
+// corrections happen via new offsetting transfers, no edits.
+
 class Category {
   final String id;
   final String name;
